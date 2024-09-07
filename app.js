162a6d8cc73dce6aa93a7a23405ec810ddcf9b59
ayusouttahere <ayus43@outlook.com>
2024-09-07 22:34:03 +0530

    fix: toggle play/pause button

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // TOGGLE VIDEO PLAY BUTTON.
 var video = document.querySelector(".hero__background") ;
 var toggleMediaButton = document.querySelector(".hero__media-control") ;
-var firstBtn = document.querySelectorAll(".hero__media-control i")[0] ;
-var secondBtn = document.querySelectorAll(".hero__media-control i")[1] ; 
+var pause = document.querySelectorAll(".hero__media-control i")[0] ;
+var play = document.querySelectorAll(".hero__media-control i")[1] ; 
 
 function togglePlay(event){
    if(video.paused){
       video.play() ; 
       console.log("played") ;
-      secondBtn.style.display = 'block' ; 
-      firstBtn.style.display = 'none' ; 
+      pause.style.display = 'none' ; 
+      play.style.display = 'block' ; 
    }else{
       video.pause() ; 
       console.log("paused") ;
-      secondBtn.style.display = 'none' ; 
-      firstBtn.style.display = 'block' ; 
+      play.style.display = 'none' ; 
+      pause.style.display = 'block' ; 
    }   }
 // adding event listener on button 
 toggleMediaButton.addEventListener("touchstart", togglePlay) ; 
