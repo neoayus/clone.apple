ca4da4640fbb24c517f9280d90736d3687d0182f
ayusouttahere <ayus43@outlook.com>
2024-09-15 12:42:59 +0530

    feat: hamburget button for mobile layout

--- app.js
+++ app.js
@@ -22,3 +22,28 @@ toggleMediaButton.addEventListener("click", togglePlay) ;
 
 //===========================================================
 // Function to handle elements on resize : width < 830px 
+   var navbar = document.querySelector(".navbar__icons") ; 
+   var lastNavBarChild = navbar.lastElementChild ;  
+
+ // creating hamburger button 
+   // document.createElement(<i class="fa-solid fa-bars"> </i>) ;  
+   let hamburgerButton = document.createElement("i") ;
+   hamburgerButton.setAttribute("class","fa-solid fa-bars") ;
+   hamburgerButton.classList.add("iicons") ; 
+
+function handleNavbarOnResize(event){
+   if(window.innerWidth < 830){
+      lastNavBarChild.append(hamburgerButton) ; 
+   }
+}
+window.addEventListener("resize", handleNavbarOnResize) ; 
+
+//===========================================================
+// adding event listener on hamburger button: 
+function expandNavbar(event){
+   console.log("button clicked! ") ;
+}
+//
+
+// 
+hamburgerButton.addEventListener("click", expandNavbar) ; 
